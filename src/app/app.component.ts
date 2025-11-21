a08349c4734b419a564dd97c06db628b54b76226
Oswaldo Hernandez <oswaldo.hernandez@insite360suite.com>
2025-11-20 22:52:10 -0300

    create experience component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HobbiesComponent } from './sections/hobbies/hobbies.component';
 import { ProjectsComponent } from './sections/projects/projects.component';
+import { ExperienceComponent } from "./sections/experience/experience.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HobbiesComponent, ProjectsComponent],
+  imports: [HobbiesComponent, ProjectsComponent, ExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
